67c30016efd25adad312565a51c688fc80cc1587
rbrin <rbrinson2@gmail.com>
2025-04-10 13:33:04 -0400

    Randomized input of testbench

--- testing/Test_tb.cpp
+++ testing/Test_tb.cpp
@@ -2,23 +2,36 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <random>
+#include <algorithm>
+#include <iterator>
 #include "verilated.h"
 #include "VTest.h"
 
 void A_Loading_Logic(std::unique_ptr<VTest> &t, std::vector<std::vector<uint32_t>> &a);
 void B_Loading_Logic(std::unique_ptr<VTest> &t, std::vector<std::vector<uint32_t>> &b);
-void MAC(std::vector<uint32_t> &a, std::vector<uint32_t> &b, std::vector<uint32_t> &c);
+void MAC(std::vector<std::vector<uint32_t>> &a, std::vector<std::vector<uint32_t>> &b, std::vector<uint32_t> &c);
 
 int main(int argc, char const *argv[])
 {
     char finished = 0;
 
-    std::vector<uint32_t> A = {1,2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::vector<uint32_t> B = {1,2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::random_device rnd_device;
+    std::mt19937 mersenne_engine {rnd_device()};  // Generates random integers
+    std::uniform_int_distribution<int> dist {1, 52};
+    auto gen = [&](){return dist(mersenne_engine);};
+
+    std::vector<uint32_t> A_rand(10);
+    std::vector<uint32_t> B_rand(10);
     std::vector<uint32_t> C, C_gold;
 
-    std::vector<std::vector<uint32_t>> A_m(3, A);
-    std::vector<std::vector<uint32_t>> B_m(3, B);
+    std::generate(A_rand.begin(), A_rand.end(), gen);
+    std::generate(B_rand.begin(), B_rand.end(), gen);
+
+    std::vector<std::vector<uint32_t>> A_m(3, A_rand);
+    std::vector<std::vector<uint32_t>> B_m(3, B_rand);
+
+    MAC(A_m, B_m, C_gold);
 
     Verilated::mkdir("logs");
     const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext}; 
@@ -67,6 +80,14 @@ int main(int argc, char const *argv[])
         std::cout << " " << i << ": " << C.at(i);
     }
     std::cout << std::endl;
+
+    for (int i = 0; i < C_gold.size(); i++){
+        std::cout << " " << i << ": " << C_gold.at(i);
+        if (C_gold.at(i) != C.at(i)) std::cout << "FAIL!";
+    }
+    std::cout << std::endl;
+
+
 
     top->final();
     return 0;
@@ -172,5 +193,18 @@ void B_Loading_Logic(
 }
 
 
-void MAC(std::vector<uint32_t> &a, std::vector<uint32_t> &b, std::vector<uint32_t> &c){
+void MAC(std::vector<std::vector<uint32_t>> &a, std::vector<std::vector<uint32_t>> &b, std::vector<uint32_t> &c){
+    int sum = 0;
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            sum = 0;
+            for (int k = 0; k < 10; k++){
+                sum += a[i][k] * b[j][k];
+            }
+            c.push_back(sum);
+        }
+    }
+    std::cout << std::endl;
+
 }
