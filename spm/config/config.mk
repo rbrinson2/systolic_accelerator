e5060233115a36fcdd3a63225823ad9ef99cad0d
rbrin <rbrinson2@gmail.com>
2025-04-11 09:08:48 -0400

    I dont know man

--- spm/config/config.mk
+++ spm/config/config.mk
@@ -6,6 +6,6 @@ export VERILOG_FILES    = $(sort $(wildcard ./designs/src/$(DESIGN_NICKNAME)/*.v
 export SDC_FILE         = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 
 export CORE_UTILIZATION = 40
-export PLACE_DENSITY    = 0.60
+export PLACE_DENSITY    = 0.0
 
 export TNS_END_PERCENT  = 100
